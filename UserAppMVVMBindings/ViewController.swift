0c7087514e32d7b918b4c3ef7a056371bc34b143
Mine Rala <mine.rala@rapsodo.com>
2022-09-09 15:23:27 +0300

    implemented observable

--- UserAppMVVMBindings/ViewController.swift
+++ UserAppMVVMBindings/ViewController.swift
@@ -7,13 +7,55 @@
 
 import UIKit
 
+// Obserable
+
+class Observable<T> {
+    var value: T? {
+        didSet {
+            listener?(value)
+        }
+    }
+    
+    init(_ value: T) {
+        self.value = value
+    }
+
+    private var listener: ((T?) -> Void)?
+    
+    private  func bind(_ listener: @escaping (T?) -> Void) {
+        listener(value)
+        self.listener = listener
+    }
+}
+
+// Model
+
+struct User: Decodable {
+    let name: String
+}
+
+// ViewModels
+
+struct UserListViewModel {
+    var users: Observable<[UserTableViewCellViewModel]> = Observable([])
+}
+
+struct UserTableViewCellViewModel  {
+    let name: String
+}
+
+// Controller
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
-
-
+    
+    
+    private func fetchData() {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/users") else { return }
+    }
 }
 
